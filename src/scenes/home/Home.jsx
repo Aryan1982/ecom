178c2c22d3d900c4b188d5c44e1768eaac5505e0
Karan Megha <92364515+KaranMegha@users.noreply.github.com>
2023-03-07 22:15:27 +0530

    Add files via upload

---
+++ src/scenes/home/Home.jsx
@@ -0,0 +1,19 @@
+import ShoppingList from "./ShoppingList";
+import Subscribe from "./Subscribe";
+import MainCarousel from "./MainCarousel";
+import Navbar from "../global/Navbar";
+import Footer from "../global/Footer";
+
+function Home() {
+  return (
+    <div className="home">
+      <Navbar/>
+      <MainCarousel />
+      <ShoppingList />
+      {/* <Subscribe /> */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
